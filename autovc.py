5f5876d772c3049bf4bf2cdeca3a1ac089aaee0a
faipena <196348511+faipena@users.noreply.github.com>
2025-02-21 19:24:23 +0000

    black

--- autovc.py
+++ autovc.py
@@ -14,41 +14,51 @@ LISTEN_PORT = 18889
 
 # Classes and functions
 
+
 def abort(message: str):
     print("ERROR", message)
     sys.exit(1)
+
 
 @dataclass
 class Model:
     index: int | str
     name: str
 
+
 def get_info(url: str):
     resp = requests.get(f"{url}/info")
     if resp.status_code != 200:
         abort("cannot retrieve information from voice changer server")
     models = [
-        Model(index=m["slotIndex"], name=m["name"]) for m in
-          filter(lambda x: x["voiceChangerType"] != None, resp.json()["modelSlots"])
+        Model(index=m["slotIndex"], name=m["name"])
+        for m in filter(
+            lambda x: x["voiceChangerType"] != None, resp.json()["modelSlots"]
+        )
     ]
     return models
+
 
 # Flask server
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins=VC_API)
 
+
 def start_server(port: int):
     socketio.run(app=app, host="127.0.0.1", port=port)
+
 
 @app.route("/start", methods=["PUT"])
 def flask_start():
     socketio.emit("vc start")
     return {"started": True}, 200
 
+
 @app.route("/stop", methods=["PUT"])
 def flask_stop():
     socketio.emit("vc stop")
     return {"started": False}, 200
+
 
 @app.after_request
 def add_cors_headers(response):
@@ -57,11 +67,14 @@ def add_cors_headers(response):
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
 
+
 # Commands
+
 
 def list(api_url: str):
     models = get_info(api_url)
     print(tabulate(models, headers=["Index", "Name"]))
+
 
 def start(port: int):
     try:
@@ -76,25 +89,35 @@ def stop(port: int):
     except requests.exceptions.ConnectionError:
         start_server(port)
 
+
 def select(api_url: str, model_index: str):
     try:
         model_index = f"{int(model_index):03}"
     except:
         model_index = f"000{model_index}"
     # The following None is used to remove filename from the multi part form
-    requests.post(f"{api_url}/update_settings", files={
-        "key": (None,"modelSlotIndex"),
-        "val": (None,f"{int(time.time())}{model_index}")
-    })
+    requests.post(
+        f"{api_url}/update_settings",
+        files={
+            "key": (None, "modelSlotIndex"),
+            "val": (None, f"{int(time.time())}{model_index}"),
+        },
+    )
+
 
 def main():
     parser = ArgumentParser("autovc", description="Voice Changer Automatization")
-    subparsers = parser.add_subparsers(dest="command", help="Command to execute", required=True)
+    subparsers = parser.add_subparsers(
+        dest="command", help="Command to execute", required=True
+    )
     _ = subparsers.add_parser("start", help="Start the voice changer")
     _ = subparsers.add_parser("stop", help="Stop the voice changer")
     _ = subparsers.add_parser("list", help="List available models")
     select_parser = subparsers.add_parser("select", help="Select a model")
-    select_parser.add_argument("INDEX", help="Model index to select, use 'list' command to get model names and indexes")
+    select_parser.add_argument(
+        "INDEX",
+        help="Model index to select, use 'list' command to get model names and indexes",
+    )
     args = parser.parse_args()
     if args.command == "list":
         list()
@@ -104,6 +127,7 @@ def main():
         stop(LISTEN_PORT)
     elif args.command == "select":
         select(args.INDEX)
+
 
 if __name__ == "__main__":
     main()
